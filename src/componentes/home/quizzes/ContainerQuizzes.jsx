58027a22530c6d30fcae39306cc02ee625eda44c
marcojr73 <marcojuniorengineer@gmail.com>
2022-08-07 21:00:29 -0300

    style list quiz

--- src/componentes/home/quizzes/ContainerQuizzes.jsx
+++ src/componentes/home/quizzes/ContainerQuizzes.jsx
@@ -1,5 +1,53 @@
 import styled from "styled-components"
 
 export const ContainerQuizzes = styled.main`
-    
+    position: absolute;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15vh;
+
+    h1{
+        text-align: center;
+    }
+
+    .settings{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        width: 100%;
+        font-size: 20px;
+    }
+
+    .button{
+        color: #FFFFFF;
+        width: 150px;
+        height: 30px;
+        background-color: var(--cor-principal);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        border-radius: 20px;
+    }
+
+    .box{
+        margin-bottom: 45px;
+        width: 550px;
+        height: 200px;
+        background-color: var(--cor-secundaria);
+        border-radius: 20px;
+        display: flex;
+    }
+
+    .banner{
+        width: 50%;
+        height: 100%;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
 ` 
--- src/componentes/home/quizzes/Quizzes.jsx
+++ src/componentes/home/quizzes/Quizzes.jsx
@@ -1,9 +1,39 @@
 import { ContainerQuizzes } from "./ContainerQuizzes";
+import mapCity from "../../../assets/images/mapcity.jpeg"
+import flags from "../../../assets/images/flags.jpeg"
+import mapCountry from "../../../assets/images/mapCountry.jpeg"
+import { Quiz } from "./Quizz";
 
 export function Quizzes(){
     return(
         <ContainerQuizzes>
-             <h1>I am the quizz</h1>
+            <div className="box">
+                <img className="banner" src={mapCity}/>
+                <div className="settings">
+                    <h1>How many capitals <br/> can you hit?</h1>
+                    <button className="button easy">Easy</button>
+                    <button className="button medium">Medium</button>
+                    <button className="button hard">Hard</button>
+                </div>
+            </div>
+            <div className="box">
+                <img className="banner" src={flags}/>
+                <div className="settings">
+                    <h1>How many flags <br/> can you hit?</h1>
+                    <button className="button easy">Easy</button>
+                    <button className="button medium">Medium</button>
+                    <button className="button hard">Hard</button>
+                </div>
+            </div>
+            <div className="box">
+                <img className="banner" src={mapCountry}/>
+                <div className="settings">
+                    <h1>How many territories <br/> can you hit?</h1>
+                    <button className="button easy">Easy</button>
+                    <button className="button medium">Medium</button>
+                    <button className="button hard">Hard</button>
+                </div>
+            </div>
         </ContainerQuizzes>
     )
 }
