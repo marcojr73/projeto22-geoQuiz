3426016f8b107840e2136c23d04944c36922c89d
marcojr73 <marcojuniorengineer@gmail.com>
2022-08-08 22:05:06 -0300

    layout and list options quiz

---
+++ src/componentes/quiz/ContainerQuiz.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+
+export const ContainerQuiz = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1{
+        margin-top: 5%;
+        font-size: 40px;
+    }
+
+    h2{
+        font-size: 30px;
+    }
+
+    .options, .single-quiz{
+        display: flex;
+        flex-direction: column;
+    }
+    .single-quiz{
+        margin-top: 5%;
+        width: 500px;
+        height: 500px;
+        background-color: var(--cor-secundaria);
+        border-radius: 25px;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    .single-option{
+        font-size: 25px;
+        margin-bottom: 15px;
+        width: 450px;
+        height: 45px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 15px;
+        background-color: #FFFFFF;
+    }
+
+
+`
--- src/componentes/quiz/Quiz.jsx
+++ src/componentes/quiz/Quiz.jsx
@@ -1,5 +1,53 @@
-function Quiz(){
+import react, { useEffect, useState } from "react"
+import { authContext } from "../../provider/authProvider"
+import axiosInstance from "../../instances/axiosInstance"
+import { Header } from "./Header"
+import { ContainerQuiz } from "./ContainerQuiz"
+
+export function Quiz(){
+
+    const token = localStorage.getItem("token")
+    const { level, quiz } = react.useContext(authContext)
+    
+    const [ position, setPosition ] = useState(0)
+    const [ questions, setQuestions ] = useState([])
+    
+    async function getQuiz(){
+        const config = {
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        }
+
+        try {
+            const response = await axiosInstance.get(`/${quiz}/${level}`, config)
+            setQuestions(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getQuiz()
+    },[])
+
     return(
-        <></>
-    )
+        questions.length > 0 ?
+        <>
+            <Header/>
+            <ContainerQuiz>
+            <h1>what is the {quiz}</h1>
+            <section className="single-quiz">
+                <h2>{questions[position].name}</h2>
+                <section className="options">
+                    {questions[position].options.map(option => {
+                        return(
+                            <div onClick={()=>setPosition( position + 1 )} className="single-option">{option}</div>
+                        )
+                    })}
+                </section>
+            </section>
+        </ContainerQuiz>
+        </>: <></>
+    ) 
 }
