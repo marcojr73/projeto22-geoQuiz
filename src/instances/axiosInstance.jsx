b13a5fabfa464c54e6cff2fd3a6edc13f7ac2f00
marcojr73 <marcojuniorengineer@gmail.com>
2022-12-18 19:00:27 -0300

    set url api

--- src/instances/axiosInstance.jsx
+++ src/instances/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://geo-quiz-api.onrender.com"
 })
 
 export default axiosInstance
